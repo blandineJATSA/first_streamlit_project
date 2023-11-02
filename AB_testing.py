375f5ba9d653534bd6e3187e455e4865c4b786d7
blandine <blandinenguetse@yahoo.com>
2023-11-02 16:57:39 +0100

    modify AB testing.py

--- AB_testing.py
+++ AB_testing.py
@@ -20,11 +20,23 @@ if selected_option == "Concevoir notre A/B testing":
              "de conversion d'une page de site Web. Cette méthode est applicable à tout type d'activités marketing.")
 
     st.write("- Mise en situation et objectifs")
-    st.write(" En tant que data scientist, nous sommes solliciter par l'équipe produit. Le concepteur a travaillé très"
-             " dur sur une nouvelle version de la page produit, dans l'espoir qu'elle entraînera un taux de conversion "
+    st.write(" En tant que data scientist, nous sommes solliciter par l'équipe produit. Le concepteur de cette équipe a "
+             "travaillé très dur sur une nouvelle version de la page produit, dans l'espoir qu'elle entraînera un taux "
+             "de conversion "
              "plus élevé. Le taux de conversion actuel est d'environ 13 % en moyenne sur l'ensemble de l'année,"
              " l'équipe aimerait une augmentation de 2 % , ce qui signifie que le nouveau design sera considéré comme "
              "un succès s'il augmente le taux de conversion à 15%."
              "Notre objectif est de tester sur un petit nombre d'utilisateurs pour voir ses performances avant de"
              " déployer le changement."
              " Nous suggérons donc d'exécuter un test A/B.")
+
+    st.write("- Objectif du test et indicateur à mesurer")
+    st.write("L'exemple d'A/B testing développé dans ce projet porte sur le design de la page produit. L'objectif"
+             " dans notre cas d'étude est d'observer comment le nouveau design de la page produit influence "
+             "le taux de conversion d'un visiteur à lead. Ainsi L'indicateur  mesuré est le taux de conversion. "
+             "la seule variable testée est le design, le texte et les couleurs restant identiques. "
+             "En effet, ce test vise à mesurer l'effet du design sur le taux de conversion"
+             )
+
+    st.write("- Hypothèses du test")
+    st.write(" ")
